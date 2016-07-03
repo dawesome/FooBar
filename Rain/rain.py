bdc72c69f7f4482b1932fa0cbefa6da209a9f3ce
Michael Dawe <mdawe@gsn.com>
2016-07-03 16:02:22 -0400

    Test solutions work, need to rework file before verification?

--- Rain/rain.py
+++ Rain/rain.py
@@ -21,3 +21,38 @@ class Hutch():
 
     def answer(self, stackList):
         self.stackList = stackList
+        start, end = self.findStartAndEndIndex(stackList)
+
+
+        standingWater = 0
+        lastHighest = 0
+        lastHighestIndex = 0
+        for x in xrange(start, end):
+            if self.stackList[x] > lastHighest:
+                lastHighest = self.stackList[x]
+                lastHighestIndex = x
+            elif self.stackList[x] < lastHighest:
+                standingWater += lastHighest - self.stackList[x]
+        if lastHighest > self.stackList[end]:
+            standingDifference = (lastHighest - self.stackList[end]) * (x - lastHighestIndex)
+            standingWater -= standingDifference
+
+
+        return standingWater
+
+    def findStartAndEndIndex(self, stacklist):
+        startHeight = 0
+        endHeight = 0
+        for x in xrange(len(stacklist) - 1):
+            if stacklist[x] > stacklist[x+1]:
+                startHeight = x
+                break
+
+        # could early out here if startheight = 0?
+
+        for x in xrange(len(stacklist) - 1, 0, -1):
+            if stacklist[x] > stacklist[x-1]:
+                endHeight = x
+                break
+
+        return startHeight, endHeight
--- Rain/rainTests.py
+++ Rain/rainTests.py
@@ -29,4 +29,36 @@ class RainTest(unittest.TestCase):
     def test_canCallAnswer(self):
         self.hutch.answer([2])
 
-    
+    def test_equalTowers(self):
+        answer = self.hutch.answer([3,1,3])
+        self.assertEqual(answer, 2)
+
+    def test_shorterThenHigher(self):
+        answer = self.hutch.answer([2,1,3])
+        self.assertEqual(answer, 1)
+
+    def test_higherThenShorter(self):
+        answer = self.hutch.answer([3,1,2])
+        self.assertEqual(answer, 1)
+
+    def test_degenerateAnswer(self):
+        answer = self.hutch.answer([1,2,3,2,1])
+        self.assertEqual(answer, 0)
+
+    def test_firstExample(self):
+        answer = self.hutch.answer([1,4,2,5,1,2,3])
+        self.assertEqual(answer, 5)
+
+    def test_findStartAndEndIndex(self):
+        start, end = self.hutch.findStartAndEndIndex([4,1,3])
+        self.assertEqual(start, 0)
+        self.assertEqual(end, 2)
+
+        start, end = self.hutch.findStartAndEndIndex([1, 4, 2, 5, 1, 2, 3])
+        self.assertEqual(start, 1)
+        self.assertEqual(end, 6)
+
+        start, end = self.hutch.findStartAndEndIndex([1,2,3,2,1])
+        self.assertEqual(start, 2)
+        self.assertEqual(end, 2)
+
